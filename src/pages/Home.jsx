93c0a08812034aca01575c0a740d05a602988c46
Alex Haight <alex.haight@gmail.com>
2023-08-28 20:26:34 -0700

    additional UI changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Footer from "components/aesthetic/Footer";
 import { callApiWithEndpoint } from "helper/callApiWithEndpoint";
 import { useEffect, useState } from "react";
 import Loading from "components/aesthetic/Loading";
-import { useParams, useNavigate, useLocation } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import Error from "./Error";
 import FolderPage from "./FolderPage";
 import ImagePage from "./ImagePage";
@@ -10,12 +10,12 @@ import Header from "components/aesthetic/Header";
 
 export default function Home(){
   const params = useParams()
-  const navigate = useNavigate()
   const location = useLocation()
   const [fileTree, setFileTree] = useState(undefined)
   const [currentChildren, setCurrentChildren] = useState({})
   const [isValidPath, setIsValidPath] = useState(true)
   const [isFolderSet, setIsFolderSet] = useState(true)
+  const [bodyColor, setBodyColor] = useState(1)
 
   const getAllObjects = async () => {
     let res = await callApiWithEndpoint('/listObjects')
@@ -65,9 +65,15 @@ export default function Home(){
     updateTreeWithLocation()
   }, [location, fileTree])
 
+  useEffect(() => {
+    let body = document.getElementsByClassName("home-page-outer")[0]
+    body.style.backgroundColor = `var(--color${bodyColor})`
+    setBodyColor(bodyColor === 7 ? 1 : bodyColor+1)
+  }, [location])
+
   if(isValidPath){
     return(
-      <div className="color-4 fit-page relative-position flex-container-column">
+      <div className="color-default fit-page relative-position flex-container-column home-page-outer">
         <Header />
         { fileTree !== undefined ? (
           isFolderSet ? (
